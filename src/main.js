8675f058241603abc287abcf4ff92a9fd5178bdb
gjlacerda <guilherme.lcd@gmail.com>
2017-06-13 22:22:36 -0300

    Removed Archive class for tests

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-const Archive  = require('./classes/Archive'),
-      Document = require('./classes/Document'),
+const Document = require('./classes/Document'),
       Movie    = require('./classes/Movie'),
       Music    = require('./classes/Music'),
       Picture  = require('./classes/Picture'),
       types    = [
-          new Archive(),
           new Document(),
           new Movie(),
           new Music(),
