23336887b511dc95dfcba4fcbf1bcb6c8aba55c9
gjlacerda <guilherme.lcd@gmail.com>
2017-06-13 22:25:31 -0300

    Added #!/usr/bin/env node

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-const Document = require('./classes/Document'),
+#!/usr/bin/env node
+
+const Archive  = require('./classes/Archive'),
+      Document = require('./classes/Document'),
       Movie    = require('./classes/Movie'),
       Music    = require('./classes/Music'),
       Picture  = require('./classes/Picture'),
       types    = [
+          new Archive(),
           new Document(),
           new Movie(),
           new Music(),
